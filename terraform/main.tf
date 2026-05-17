5a9f5a544cb5d35ba95b1b327ebf24f3b237a79b
Chris Duke <duke@drakkon.net>
2026-05-17 11:36:38 -0700

    chore: remove Terraform configuration files and add initial HTML document

--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-# Main Terraform configuration
-# This file imports all the infrastructure components
-
-# CloudFront distribution ID
-locals {
-  distribution_id = aws_cloudfront_distribution.website.id
-  bucket_name     = aws_s3_bucket.website.id
-}
